9cdb9c043623ecca29493aad028fed93d438b080
mardonov03 <125536221+mardonov03@users.noreply.github.com>
2025-03-23 05:17:54 +0500

    new chat member 1

--- tgbot/models/config.py
+++ tgbot/models/config.py
@@ -11,14 +11,56 @@ class MainModel:
         self.bot = bot
 
 class UsersModel(MainModel):
-    async def get_user(self, userid):
-        async with self.pool.acquire() as conn:
-            result = await conn.fetchrow('SELECT * FROM users WHERE userid = $1', userid)
-            if result:
-                return {"userid": result["userid"], "username": result["username"]}
-            return None
+    async def __add_user(self, userid):
+        try:
+            user = await self.bot.get_chat(userid)
+            username = user.username if user.username else None
+            name = user.first_name
 
-    async def __user_exists(self, user_id: int) -> bool:
-        async with self.pool.acquire() as conn:
-            user_record = await conn.fetch('SELECT userid FROM users WHERE userid = $1', user_id)
-            return bool(user_record)
+            async with self.pool.acquire() as conn:
+                await conn.execute('INSERT INTO users (userid, username, name) VALUES ($1, $2, $3)', userid, username, name)
+
+            return {'userid': userid, 'username': username, 'name': name}
+
+        except Exception as e:
+            logging.error(f'__add_user error: {e}')
+
+    async def get_user(self, userid):
+        try:
+            async with self.pool.acquire() as conn:
+                result = await conn.fetchrow('SELECT * FROM users WHERE userid = $1', userid)
+                if not result:
+                    result = await self.__add_user(userid)
+                return {"userid": result["userid"], "username": result["username"], "name": result['name']}
+        except Exception as e:
+            logging.error(f'"get_user error: {e}')
+
+class GroupModel(MainModel):
+    async def add_group(self, groupid):
+        try:
+            chat = await self.bot.get_chat(groupid)
+            username = chat.username if chat.username else None
+            name = chat.title
+
+            async with self.pool.acquire() as conn:
+                await conn.execute('INSERT INTO groups (groupid, username, name) VALUES ($1, $2, $3)', groupid, username, name)
+
+            return {'groupid': groupid, 'username': username, 'name': name}
+
+        except Exception as e:
+            logging.error(f'__add_group error: {e}')
+
+    async def get_group(self, groupid):
+        try:
+            async with self.pool.acquire() as conn:
+                result = await conn.fetchrow('SELECT * FROM groups WHERE groupid = $1', groupid)
+            return result
+        except Exception as e:
+            logging.error(f'"get_user error: {e}')
+
+    async def turn_on_off_bot(self, groupid, newstatus: bool):
+        try:
+            async with self.pool.acquire() as conn:
+                await conn.execute('UPDATE group_states SET (group_status = $1) WHERE groupid = $2', newstatus, groupid)
+        except Exception as e:
+            logging.error(f'"turn-on_bot error": {e}')
